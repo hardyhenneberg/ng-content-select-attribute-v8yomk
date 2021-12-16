4e6288a70680d7a6fdee64010fe18e0c4c64d38d
hardyhenneberg <hh@hhenne.dk>
2021-12-16 09:43:44 +0100

    2 files modified

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,9 +6,10 @@ import { AppComponent } from './app.component';
 import { HelloComponent } from './hello.component';
 import { ChildComponent } from '.././child/child.component';
 import { MatCardModule } from '@angular/material/card';
+import { MatToolbarModule } from '@angular/material/toolbar/toolbar-module';
 
 @NgModule({
-  imports: [BrowserModule, FormsModule, MatCardModule],
+  imports: [BrowserModule, FormsModule, MatCardModule, MatToolbarModule],
   declarations: [AppComponent, HelloComponent, ChildComponent],
   bootstrap: [AppComponent],
 })
